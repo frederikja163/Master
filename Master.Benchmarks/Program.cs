0b174093600f1842c9a5445e897944f31e8824c8
Frederikja <fjaplv64@gmail.com>
2025-09-04 22:55:40 +0200

    Initial commit

---
+++ Master.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Master.Benchmarks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# OpenTAP result file format
+
+This is currently work in progress.
+
+Authors @Aavild and @frederikja163 for their masters project at AAU
