3399c0b03c232d8651e325ea1b961a12ba95c620
Frederikja <fjaplv64@gmail.com>
2026-02-17 11:54:22 +0100

    Fix unit tests again

--- Master.Benchmarks/Program.cs
+++ Master.Benchmarks/Program.cs
@@ -1,6 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
-using Master.Benchmarks.Data;
-using Master.Benchmarks.Raw;
 
 namespace Master.Benchmarks;
 
@@ -8,25 +6,9 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        if (args[0] == "t")
-        {
-            foreach (TpchData data in TPCHBenchmarks.GetData().ToArray())
-            {
-                TPCHBenchmarks b = new TPCHBenchmarks()
-                {
-                    Data = data,
-                };
-                b.WriteRaw(new CascadingBenchmark());
-            }
-        }
-
-        
-        else if (args[0] == "b")
-        {
-            // BenchmarkRunner.Run<OpenTAPBenchmarks>();
-            // BenchmarkRunner.Run<RawBenchmarks>();
-            // BenchmarkRunner.Run<SparkBenchmarks>();
-            BenchmarkRunner.Run<TPCHBenchmarks>();
-        }
+        BenchmarkRunner.Run<OpenTAPBenchmarks>();
+        BenchmarkRunner.Run<RawBenchmarks>();
+        BenchmarkRunner.Run<SparkBenchmarks>();
+        BenchmarkRunner.Run<TPCHBenchmarks>();
     }
 }
--- Master/Serializing/DataColumnBuilder.cs
+++ Master/Serializing/DataColumnBuilder.cs
@@ -67,7 +67,9 @@ internal ref struct DataColumnBuilder
             case double float64: BinaryPrimitives.WriteDoubleLittleEndian(slice, float64); break;
             default: throw new ArgumentOutOfRangeException(nameof(T), typeof(T), null);
         }
-        Debug.Assert(_type.TryGetSize(out int size));
+
+        if (!_type.TryGetSize(out int size))
+            size = Unsafe.SizeOf<T>();
         _logicalLength += Unsafe.SizeOf<T>() / size;
     }
 
