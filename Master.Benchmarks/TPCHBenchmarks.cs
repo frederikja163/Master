d7091e047f72f27418ffaae833e718843c9ba512
Aavild <anders@aavild.dk>
2026-02-20 08:53:29 +0100

    final revert

--- Master.Benchmarks/TPCHBenchmarks.cs
+++ Master.Benchmarks/TPCHBenchmarks.cs
@@ -110,12 +110,12 @@ public class TPCHBenchmarks
         yield return new CascadingBenchmark();
         yield return new CascadingAsyncBenchmark();
         yield return new RawParquet(CompressionMethod.Snappy);
-        yield return new RawParquet(CompressionMethod.Zstd);
-        yield return new RawParquet(CompressionMethod.Gzip);
+        // yield return new RawParquet(CompressionMethod.Zstd);
+        // yield return new RawParquet(CompressionMethod.Gzip);
         yield return new RawParquet(CompressionMethod.None);
-        yield return new RawParquet(CompressionMethod.LZ4);
-        yield return new RawParquet(CompressionMethod.Lz4Raw);
-        yield return new RawParquet(CompressionMethod.Brotli);
+        // yield return new RawParquet(CompressionMethod.LZ4);
+        // yield return new RawParquet(CompressionMethod.Lz4Raw);
+        // yield return new RawParquet(CompressionMethod.Brotli);
         yield return new RawCsv();
         yield return new RawSqlite();
         yield return new RawHdf5Benchmark();
