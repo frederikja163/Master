db97e53e18d3f22149fd142182e7fac51504e513
Frederikja <fjaplv64@gmail.com>
2026-02-23 23:04:30 +0100

    Wip

---
+++ Master/Serializing/Readers/BitPackingColumnReader.cs
@@ -0,0 +1,102 @@
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
+using Master.Serializing.Columns;
+
+namespace Master.Serializing.Readers;
+
+internal sealed class BitPackingColumnReader<T> : IColumnReader<T>
+    where T : unmanaged, INumber<T>, IBinaryInteger<T>, IMinMaxValue<T>
+{
+    public byte PrefixLength { get; }
+    public ulong Prefix { get; }
+    public LogicalType Type { get; }
+    public IColumnReader<T> ColumnReader { get; }
+    public int Length { get; }
+    public int Index { get; } = 0;
+    private readonly int _valueBitSize;
+
+    public BitPackingColumnReader(byte prefixLength, ulong prefix, int logicalLength, LogicalType type, IColumnReader<T> reader)
+    {
+        PrefixLength = prefixLength;
+        Prefix = prefix;
+        Length = logicalLength;
+        Type = type;
+        ColumnReader = reader;
+    }
+    
+    public T Peek(int offset = 0)
+    {
+    }
+
+    public IEnumerable<T> Peek(int count, int offset)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Advance(int units)
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+    public DataColumn Decode(IColumn data)
+    {
+        if (data is not BitPackingColumn bitPackingColumn)
+            throw new Exception($"Data({nameof(data)}) is not a BitPackingColumn");
+        DataColumn dataColumn = (DataColumn) bitPackingColumn.Column; // TODO: needs to not be casted here
+        if (!bitPackingColumn.Type.TryGetSize(out int size))
+        {
+            throw new Exception("Type must be a primitive");
+        }
+        
+        DataColumn column = size switch
+        {
+            1 => Decode<byte>(dataColumn, bitPackingColumn),
+            2 => Decode<ushort>(dataColumn, bitPackingColumn),
+            4 => Decode<uint>(dataColumn, bitPackingColumn),
+            8 => Decode<ulong>(dataColumn, bitPackingColumn),
+            _ => throw new Exception("Logical type size must be either 1, 2, 4 or 8."),
+        };
+        return column;
+    }
+
+    private DataColumn Decode<T>(DataColumn dataColumn, BitPackingColumn metadata)
+        where T : unmanaged, INumber<T>, IBinaryInteger<T>, IMinMaxValue<T>
+    {
+        int size = Unsafe.SizeOf<T>() * 8;
+        int packedSize = size - metadata.PrefixLength;
+        int length = metadata.LogicalLength;
+        DataColumnReader reader = dataColumn.OpenReader();
+        DataColumnBuilder builder =
+            new DataColumnBuilder(metadata.Type, length * size / 8);
+
+        T flag = (T.MaxValue << metadata.PrefixLength) >> metadata.PrefixLength;
+        ulong p = metadata.Prefix << (size - metadata.PrefixLength);
+        T prefix = Unsafe.As<ulong, T>(ref p);
+        T currentValue = reader.Read<T>();
+        int shift = size - packedSize;
+        for (int i = 0; i < length; i++)
+        {
+            T value;
+            if (shift >= 0)
+            {
+                value = (currentValue >> shift);
+            }
+            else
+            {
+                int shift1 = int.Abs(shift);
+                value = currentValue << shift1;
+                currentValue = reader.Read<T>();
+                shift = size - shift1;
+                value |= (currentValue >> shift);
+            }
+
+            value &= flag;
+            value |= prefix;
+            builder.Write(value);
+            shift -= packedSize;
+        }
+        
+        return builder.Build();
+    }
+}
---
+++ Master/Serializing/Readers/SplitColumnReader.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace Master.Serializing.Readers;
+
+internal sealed class SplitColumnReader : IColumnReader<string>, IColumnReader<byte[]>
+{
+    private readonly IColumnReader<int> _lengthColumn;
+    private readonly IColumnReader<byte> _byteColumn;
+
+    public int Length => _lengthColumn.Length;
+    public int Index => _lengthColumn.Index;
+
+    public void Advance(int units)
+    {
+        for (int i = 0; i < units; i++)
+        {
+            int length = _lengthColumn.Read();
+            _byteColumn.Advance(length);
+        }
+    }
+
+    string IColumnReader<string>.Peek(int offset)
+    {
+        int length = _lengthColumn.Peek(offset);
+        return Encoding.UTF8.GetString(_byteColumn.Peek(length, offset).ToArray());
+    }
+
+    IEnumerable<string> IColumnReader<string>.Peek(int count, int offset)
+    {
+        IColumnReader<string> stringReader = this;
+        for (int i = 0; i < count; i++)
+        {
+            yield return stringReader.Peek(offset + i);
+        }
+    }
+
+    byte[] IColumnReader<byte[]>.Peek(int offset)
+    {
+        int length = _lengthColumn.Peek(offset);
+        return _byteColumn.Peek(length, offset).ToArray();
+    }
+
+    IEnumerable<byte[]> IColumnReader<byte[]>.Peek(int count, int offset)
+    {
+        IColumnReader<byte[]> blobReader = this;
+        for (int i = 0; i < count; i++)
+        {
+            yield return blobReader.Peek(offset + i);
+        }
+    }
+}
