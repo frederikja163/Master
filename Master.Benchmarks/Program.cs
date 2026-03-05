69161a40a804cc5fff56c0eac5dfe865474be878
Frederikja <fjaplv64@gmail.com>
2026-03-05 12:12:06 +0100

    update master.benchmarks

--- Master.Benchmarks/Program.cs
+++ Master.Benchmarks/Program.cs
@@ -7,11 +7,9 @@ internal static class Program
 {
     internal static void Main(string[] args)
     {
-        var b = new TPCHBenchmarks() { Data = TPCHBenchmarks.GetData().First(t => t.ToString() == "LINEITEM") };
-        b.WriteRaw(new CascadingBenchmark());
-        /*BenchmarkRunner.Run<OpenTAPBenchmarks>();
+        BenchmarkRunner.Run<OpenTAPBenchmarks>();
         BenchmarkRunner.Run<RawBenchmarks>();
-        BenchmarkRunner.Run<SparkBenchmarks>();*/
-        // BenchmarkRunner.Run<TPCHBenchmarks>();
+        BenchmarkRunner.Run<SparkBenchmarks>();
+        BenchmarkRunner.Run<TPCHBenchmarks>();
     }
 }
