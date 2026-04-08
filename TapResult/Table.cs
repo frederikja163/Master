120b3efc394c0ea824237f454d1d77f6a397171e
Frederikja <fjaplv64@gmail.com>
2026-04-08 22:44:21 +0200

    Make result listener

--- TapResult/Table.cs
+++ TapResult/Table.cs
@@ -79,7 +79,7 @@ public sealed class Table : IColumnParent
         await Task.WhenAll(tasks);
     }
 
-    internal Table(IEnumerable<IColumn> columns, IEnumerable<string> names, string name)
+    public Table(IEnumerable<IColumn> columns, IEnumerable<string> names, string name)
     {
         _columns = columns.ToArray();
         _names = names.ToArray();
